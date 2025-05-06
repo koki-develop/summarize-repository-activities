373163a1a7211087ba458fa5be4faee8fd99b21c
koki-develop <kou.pg.0131@gmail.com>
2025-05-06 17:33:21 +0900

    Implement utilities

---
+++ src/lib/util.ts
@@ -0,0 +1,18 @@
+import { format } from "date-fns";
+
+export function removeChecklist(text: string): string {
+  const checklistRegex = /^[\s]*- \[[x ]\].+(\r?\n|$)/gm;
+  return text.replace(checklistRegex, "");
+}
+
+export function removeCodeblock(markdown: string): string {
+  return markdown.replace(/^```[\s\S]*?```(\r?\n|$)/gm, "");
+}
+
+export function removeComment(markdown: string): string {
+  return markdown.replace(/^<!--[\s\S]*?-->(\r?\n|$)/gm, "");
+}
+
+export function yyyymmdd(date: Date): string {
+  return format(date, "yyyy/MM/dd");
+}
